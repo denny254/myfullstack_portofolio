cccf741347d413a2324b85988765682459329642
KINANGA DENNIS <denniskinanga6@gmail.com>
2023-11-10 16:59:22 +0300

    vercel

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,3 @@
-source "D:/Django projects/KinangaSite/env/Scripts/activate.bat"
-
 pip install -r requirements.txt
 
-pip list
-
-python 3.9 manage.py collectstatic --noinput
+python 3.9 manage.py collectstatic

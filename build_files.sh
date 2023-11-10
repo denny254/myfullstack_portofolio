5aebfabb411cf0c8cc8e3efbc9cac2fb473feb6d
KINANGA DENNIS <denniskinanga6@gmail.com>
2023-11-10 17:40:40 +0300

    vercel

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,4 @@
 
-source "D:/Django projects/KinangaSite/env/Scripts/activate"
-
-
-echo "Installing/upgrading pip..."
-python -m pip install --upgrade pip
-
-echo "Installing dependencies..."
 pip install -r requirements.txt
-
-echo "Installed packages:"
-pip list
-
-echo "Running collectstatic..."
 python manage.py collectstatic --noinput
 

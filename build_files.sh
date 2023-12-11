16bdafeb19d03d101178c17d17e7903bb87446f8
KINANGA DENNIS <denniskinanga6@gmail.com>
2023-12-11 13:09:17 +0300

    add

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,10 @@
+echo "Building project packages..."
+python3 -m pip install -r requirements.txt
 
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+echo "Migrating Database..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput
 

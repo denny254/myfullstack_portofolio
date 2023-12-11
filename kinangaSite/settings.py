f6b1645bc966782458ab127ffa3458ccb3f4906c
KINANGA DENNIS <denniskinanga6@gmail.com>
2023-12-11 14:52:28 +0300

    add

--- kinangaSite/settings.py
+++ kinangaSite/settings.py
@@ -131,12 +131,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+# STATIC_URL = "/static/"
 MEDIA_URL = "/images/"
+STATIC_URL = "/staticfiles/"
 
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
+
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
